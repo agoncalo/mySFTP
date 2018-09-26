0c873a86156174474d4d59fc10a5c9f39eb1f7b7
antonio <antoniomarciofilho@gmail.com>
2018-09-26 20:42:48 -0300

    Adding Impl inner servers

--- src/mysftp/password_server/PasswordServerImpl.java
+++ src/mysftp/password_server/PasswordServerImpl.java
@@ -1,5 +1,9 @@
 package mysftp.password_server;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import mysftp.rpc_api.PasswordServer;
 
 public class PasswordServerImpl implements PasswordServer {
@@ -24,6 +28,20 @@ public class PasswordServerImpl implements PasswordServer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String host = args[0] != null ? args[0] : null;
+		int port = args[1] != null ? Integer.parseInt(args[1]) : null;
+		
+		try {
+			PasswordServerImpl server = new PasswordServerImpl();
+			PasswordServer stub = (PasswordServer)UnicastRemoteObject.exportObject(server, port);
+			
+			Registry registry = LocateRegistry.getRegistry(host, port);
+			registry.bind("PasswordServer", stub);
+			
+			System.err.println("Password Server ready @ " + host + " : " + port);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 
